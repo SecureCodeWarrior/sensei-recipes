5f36984733315eeb35837bfce543f1dfd7c72337
Nick Van Haver <nhaver@securecodewarrior.com>
2021-08-18 14:04:13 +0200

    Reviewed broken recipes, corrected tags, minor fixes

---
+++ example_code/java-basic-examples/src/ClosedStreams.java
@@ -0,0 +1,9 @@
+import java.io.BufferedInputStream;
+
+public class ClosedStreams {
+    public static void main(String[] args) {
+        BufferedInputStream bis = new BufferedInputStream(System.in);
+
+
+    }
+}
--- example_code/java-basic-examples/src/Equality.java
+++ example_code/java-basic-examples/src/Equality.java
@@ -3,11 +3,23 @@ import java.util.Objects;
 public class Equality {
 
     public static void main(String[] args) {
-        boolean p1 = Objects.equals(Integer.valueOf(2),2); // true
-        boolean p2 = Objects.equals(2, Integer.valueOf(2)); // true
+        Object o = new Object();
+        boolean[] p = new boolean[11];
 
-        boolean p3 = Objects.deepEquals(Integer.valueOf(2),2); // true
-        boolean p4 = Objects.deepEquals(2, Integer.valueOf(2)); // true
+        p[1] = Objects.equals(Integer.valueOf(2),2); // true
+        p[2] = Objects.equals(2, Integer.valueOf(2)); // true
+        p[3] = Objects.deepEquals(Integer.valueOf(2),2); // true
+        p[4] = Objects.deepEquals(2, Integer.valueOf(2)); // true
+        p[5] = Objects.equals(null, null); // true
+        p[6] = Objects.equals(null, o); // false
+        p[7] = Objects.equals(o, null); // false
+        p[8] = Objects.equals(o, o); // true
+        p[9] = Objects.equals(o, 1); // false
+        p[10] = Objects.equals(1, o); // false
+
+        for (int i = 1; i < p.length; i++) {
+            System.out.println("p["+i+"] = " + p[i]);
+        }
     }
 
 }
