e2e96d07e4cff5da9fbd62716fb8c44b2ed9ad8c
Nick Van Haver <nhaver@securecodewarrior.com>
2021-10-05 13:12:40 +0200

    Added recipe to add private constructor on class with only static methods and fields

---
+++ example_code/pojoexamples/src/main/java/PrivateConstructor.java
@@ -0,0 +1,15 @@
+public class PrivateConstructor {
+
+    public static String test = "test";
+    public static final String test2 = "test2";
+    //private static String test3 = "test3";
+    //private String test4 = "test4";
+    public static String test5 = "test5";
+
+    //public PrivateConstructor(String test4) {}
+    //private PrivateConstructor() { }
+
+    public static void doSomething() {}
+    private static void doSomethingElse() {}
+    //private void doSomethingElse2() {}
+}
