984d2b6694385b7a0d7a250df58cd4e5964a7466
Cameron Gregor <cgregor@securecodewarrior.com>
2021-11-17 10:49:20 +1100

    Update getCenturyOfEra recipe

--- example_code/joda-time-to-java-time-examples/src/test/java/common/GetCenturyFieldsTest.java
+++ example_code/joda-time-to-java-time-examples/src/test/java/common/GetCenturyFieldsTest.java
@@ -1,16 +1,27 @@
 package common;
 
-import org.assertj.core.api.Assertions;
+import org.joda.time.DateMidnight;
 import org.joda.time.DateTime;
+import org.joda.time.MutableDateTime;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GetCenturyFieldsTest {
+
+    /*
+        This method is used to assert that the fix provided in getCenturyOfEra.yaml
+        will correctly determine the expected century of era
+     */
+    private void test_centuryOfEra_algorithm(int expectedCenturyOfEra, int javaYear) {
+
+        int centuryOfEra = Math.abs(javaYear / 100);
+        assertThat(centuryOfEra).isEqualTo(expectedCenturyOfEra);
+
+    }
 
     /*
         Test the algorithm used by the getCenturyOfEra recipe over a 10,000 year period
@@ -18,7 +29,7 @@ public class GetCenturyFieldsTest {
     @Test
     public void testCenturyOfEra() {
 
-        IntStream.range(-5000, 5000).forEach(i -> test_centuryOfEra_algorithm(i));
+        IntStream.range(-11000, 11000).forEach(i -> test_centuryOfEra_algorithm(i));
 
     }
 
@@ -28,7 +39,7 @@ public class GetCenturyFieldsTest {
     @Test
     public void testYearOfCentury() {
 
-        IntStream.range(-5000, 5000).forEach(i -> test_yearOfCentury_algorithm(i));
+        IntStream.range(-11000, 11000).forEach(i -> test_yearOfCentury_algorithm(i));
 
     }
 
@@ -38,15 +49,29 @@ public class GetCenturyFieldsTest {
     */
     private void test_centuryOfEra_algorithm(int year) {
 
+        // DateTime
         DateTime dt = DateTime.now().withYear(year);
+        DateMidnight dm = DateMidnight.now().withYear(year);
+        MutableDateTime mdt = MutableDateTime.now();
+        mdt.setYear(year);
         ZonedDateTime zdt = ZonedDateTime.now().withYear(year);
+        test_centuryOfEra_algorithm(dt.getCenturyOfEra(), zdt.getYear());
+        test_centuryOfEra_algorithm(mdt.getCenturyOfEra(), zdt.getYear());
+        test_centuryOfEra_algorithm(dm.getCenturyOfEra(), zdt.getYear());
 
-        int centuryOfEra = dt.getCenturyOfEra();
-        int zdtCenturyOfEra = Math.abs(zdt.getYear() / 100);
+        // Local Date
+        org.joda.time.LocalDate jodaLocalDate = org.joda.time.LocalDate.now().withYear(year);
+        java.time.LocalDate javaLocalDate = java.time.LocalDate.now().withYear(year);
+        test_centuryOfEra_algorithm(jodaLocalDate.getCenturyOfEra(), javaLocalDate.getYear());
 
-        assertThat(centuryOfEra).isEqualTo(zdtCenturyOfEra);
+        // Local Date Time
+        org.joda.time.LocalDateTime jodaLocalDateTime = org.joda.time.LocalDateTime.now().withYear(year);
+        java.time.LocalDateTime javaLocalDateTime = java.time.LocalDateTime.now().withYear(year);
+        test_centuryOfEra_algorithm(jodaLocalDate.getCenturyOfEra(), javaLocalDate.getYear());
+
 
     }
+
 
     /*
      * For a given year, tests that the Joda-Time method getYearOfCentury() is equivalent to the algorithm used by
