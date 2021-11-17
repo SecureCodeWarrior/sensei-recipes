51097f28665e306856465902926534edf918b2fb
Cameron Gregor <cgregor@securecodewarrior.com>
2021-11-17 11:31:24 +1100

    Fix LocalDateExamplesTest

--- example_code/joda-time-to-java-time-examples/src/test/java/notimezone/LocalDateExamplesTest.java
+++ example_code/joda-time-to-java-time-examples/src/test/java/notimezone/LocalDateExamplesTest.java
@@ -1,6 +1,5 @@
 package notimezone;
 
-import notimezone.LocalDateExamples;
 import org.junit.jupiter.api.Test;
 
 public class LocalDateExamplesTest {
@@ -10,13 +9,6 @@ public class LocalDateExamplesTest {
     public void localdate_created_with_constructors_should_match_target_date() {
 
 
-        LocalDateExamples examples = new LocalDateExamples();
-
-        /*
-            Idea here is to make all the LocalDate examples return a date, and assert they are the same
-            then the tests can be run before and after applying the recipes to make sure they all still
-            return the same date
-         */
 
     }
 
