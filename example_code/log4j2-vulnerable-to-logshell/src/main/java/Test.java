3d0d0638074f637bb208ad00c5a6b182defc3b17
Sean Flanigan <sflanigan@securecodewarrior.com>
2021-12-13 12:36:31 +1000

    SEN-2588 Add recipe to detect Log4Shell/CVE-2021-44228 (#40)
    Co-authored-by: Cameron Gregor <cgregor@securecodewarrior.com>
    Co-authored-by: Wander Neto <wneto@securecodewarrior.com>

---
+++ example_code/log4j2-vulnerable-to-logshell/src/main/java/Test.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Test {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        LOGGER.info("hello");
+    }
+}
